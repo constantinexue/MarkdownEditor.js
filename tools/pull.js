8923814e4d3a94ad96e4662137527243c152f9fd
kai.xue <kai.xue@leediancn.com>
2013-12-16 20:35:08 +0800

    OK, update pull.js, continue work on mac

--- tools/pull.js
+++ tools/pull.js
@@ -2,25 +2,7 @@ var fs = require('fs'),
     fse = require('fs-extra'),
     path = require('path'),
     request = require('request'),
-    when = require('when'),
-    pipeline = require('when/pipeline'),
-    parallel = require('when/parallel'),
-    rest = require('rest'),
     async = require('async');
-
-function Puller(url, filename) {
-    var url = url,
-        filename = filename;
-    this.pull = function() {
-        //var deferred = when.defer();
-        // request(url, function(err, response, body) {
-        //     console.log(filename);
-        //     fs.writeFileSync(filename, body);
-        //     return deferred.resolve();
-        // });
-        //return deferred.promise;
-    };
-}
 
 function pullAceLibs() {
     var lines = fs.readFileSync('ace.list', 'utf8').split('\n'),
@@ -34,24 +16,14 @@ function pullAceLibs() {
             filename = path.join(__dirname, '../public/js/ace', path.basename(line)),
             puller = new Puller(line, filename);
         request(url, function(err, response, body) {
-            results.push({
-                filename: filename,
-                content: body
-            });
+            fs.writeFileSync(filename, body, 'utf8');
             callback();
         });
     }, function(err) {
         if (err) {
             console.log(err);
         } else {
-            console.log(results.length);
-            async.eachSeries(results, function(result, callback) {
-                //console.log(typeof result.content);
-                fs.writeFileSync(result.filename, result.content, 'utf8');
-                callback();
-            }, function(err) {
-                if (err) console.log(err);
-            });
+            console.log('finished');
         }
     });
 }
